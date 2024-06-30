62c992b57295e9169eeb66dd963948982cdf195f
isaac <gladisaac5441@gmail.com>
2024-07-01 06:54:53 +0900

    auto

--- main.cpp
+++ main.cpp
@@ -16,32 +16,5 @@ int main()
     int N;
     cin >> N;
     stack<int> s;
-    while (N--) {
-        string cmd;
-        cin >> cmd;
-        if (cmd == "push") {
-            int num;
-            cin >> num;
-            s.push(num);
-        } else if (cmd == "pop") {
-            if (s.empty())
-                cout << -1 << endl;
-            else {
-                cout << s.top() << endl;
-                s.pop();
-            }
-        } else if (cmd == "size") {
-            cout << s.size() << endl;
-        } else if (cmd == "empty") {
-            if (s.empty())
-                cout << 1 << endl;
-            else
-                cout << 0 << endl;
-        } else if (cmd == "top") {
-            if (s.empty())
-                cout << -1 << endl;
-            else
-                cout << s.top() << endl;
-        }
-    }
+    cout << 1 << endl;
 }
