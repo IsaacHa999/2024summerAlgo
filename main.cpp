20470502b6e08331f20de44225b10f2f392ce211
isaac <gladisaac5441@gmail.com>
2024-07-08 23:17:11 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// boj 10814 나이순 정렬
+// boj 11725 트리의 부모 찾기
 #pragma GCC optimize("O3")  //
 
 #include <bits/stdc++.h>
@@ -15,20 +15,39 @@ int main()
 
     int N;
     cin >> N;
-    vector<pair<int, string>> v(N);
-    for (int i = 0; i < N; i++)
+    vector<vector<int>> adj(N + 1); // 인접리스트
+    vector<int> parent(N + 1);    // 부모노드
+
+    // 트리 입력
+    for (int i = 0; i < N - 1; i++) // 트리의 간선
     {
-        cin >> v[i].first >> v[i].second;
+        int a, b;
+        cin >> a >> b;
+        adj[a].push_back(b);
+        adj[b].push_back(a);
     }
 
-    // stable_sort : 정렬 전의 순서가 유지되는 정렬
-    stable_sort(v.begin(), v.end(), [](pair<int, string> a, pair<int, string> b) {
-        return a.first < b.first;
-    });
-
-    // 출력
-    for (int i = 0; i < N; i++)
+    // bfs
+    queue<int> q;
+    q.push(1);
+    parent[1] = 1;
+    while (!q.empty())
     {
-        cout << v[i].first << " " << v[i].second << endl;
+        int cur = q.front();    // 현재 노드
+        q.pop();
+        for (int next : adj[cur])   // 인접노드
+        {
+            if (parent[next] == 0)  // 방문하지 않은 노드
+            {
+                parent[next] = cur; // 부모노드 저장
+                q.push(next);    // 큐에 삽입
+            }
+        }
+    }
+
+    // print
+    for (int i = 2; i <= N; i++)
+    {
+        cout << parent[i] << endl;
     }
 }
