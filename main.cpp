944f60c85747ca2f07a0fe4b1cc72c09f92bb816
isaac <gladisaac5441@gmail.com>
2024-07-16 09:18:57 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-// boj 11724 연결 요소의 개수
+// boj 10250 ACM 호텔
+
 #pragma GCC optimize("O3")
 
 #include <bits/stdc++.h>
@@ -7,49 +8,25 @@
 #define ll long long
 using namespace std;
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N, M;
-    cin >> N >> M;
-    vector<vector<int>> adj(N + 1);
-    vector<bool> visited(N + 1, false);
+    int T;
+    cin >> T;
+    while (T--) {
+        int H, W, N;
+        cin >> H >> W >> N;
+        int floor = N % H;
+        int room = N / H + 1;
 
-    //
-    for (int i = 0; i < M; i++)
-    {
-        int u, v;
-        cin >> u >> v;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-
-    // BFS
-    int cnt = 0;
-    for (int i = 1; i <= N; i++)
-    {
-        if (visited[i])
-            continue;
-        cnt++;
-        queue<int> q;
-        q.push(i);
-        visited[i] = true;
-        while (!q.empty())
-        {
-            int cur = q.front();
-            q.pop();
-            for (int next : adj[cur])
-            {
-                if (visited[next])
-                    continue;
-                visited[next] = true;
-                q.push(next);
-            }
+        if (floor == 0) {
+            floor = H;
+            room--;
         }
+        cout << floor * 100 + room << endl;
     }
-
-    // Output
-    cout << cnt << endl;
 }
+
+
+
