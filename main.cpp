0477807b8d05d9a5b8d7bfbea83426141a0b03df
isaac <gladisaac5441@gmail.com>
2024-07-17 09:36:38 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
-// boj 10250 ACM 호텔
+// boj 1181 단어 정렬
+// 길이가 짧은 것부터
+// 길이가 같으면 사전 순으로
 
 #pragma GCC optimize("O3")
 
@@ -12,19 +14,27 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int T;
-    cin >> T;
-    while (T--) {
-        int H, W, N;
-        cin >> H >> W >> N;
-        int floor = N % H;
-        int room = N / H + 1;
+    int N;
+    cin >> N;
+    vector<string> v(N);
 
-        if (floor == 0) {
-            floor = H;
-            room--;
+    for (int i = 0; i < N; i++) {
+        cin >> v[i];
+    }
+
+    // 길이가 짧은 것부터, 길이가 같으면 사전 순으로
+    sort(v.begin(), v.end(), [](const string &a, const string &b) {
+        if (a.size() == b.size()) {
+            return a < b;
         }
-        cout << floor * 100 + room << endl;
+        return a.size() < b.size();
+    });
+
+    // 중복 제거
+    v.erase(unique(v.begin(), v.end()), v.end());   // 중복 제거, unique는 중복된 원소를 맨 뒤로 보내고 중복되지 않은 원소의 시작 주소를 반환
+    // 출력
+    for (const auto &s : v) {
+        cout << s << endl;
     }
 }
 
