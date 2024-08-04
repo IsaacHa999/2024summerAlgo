dffb0aed00ae0190b97e7eddad6340ee525fce72
isaac <gladisaac5441@gmail.com>
2024-08-05 08:56:17 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// boj 14888 연산자 끼워넣기
+// boj 14425 문자열 집합
 #pragma GCC optimize("O3")
 
 #include <bits/stdc++.h>
@@ -8,58 +8,34 @@
 using namespace std;
 
 // 함수 선언
-void dfs(int idx, int val, vector<int> &A, vector<int> &op, int N); // idx: 현재 인덱스, val: 현재까지의 계산값, A: 숫자 배열, op: 연산자 배열, N: 숫자 개수
 
-int max_val = INT_MIN;
-int min_val = INT_MAX;
+// 전역 변수
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N;
-    cin >> N;
-    vector<int> A(N);
-    for (int i = 0; i < N; i++)
-        cin >> A[i];
-    vector<int> op(4);
-    for (int i = 0; i < 4; i++)
-        cin >> op[i];
+    // 입력
+    int N, M;
+    cin >> N >> M;
+    set<string> S;  // 문자열 집합
 
-    // dfs
-    dfs(1, A[0], A, op, N); // idx: 1, val: A[0], A: A, op: op, N: N
-
-    // 결과 출력
-    cout << max_val << endl;
-    cout << min_val << endl;
-
-
-}
-
-void dfs(int idx, int val, vector<int> &A, vector<int> &op, int N)
-{
-    if (idx == N)
-    {
-        max_val = max(max_val, val);
-        min_val = min(min_val, val);
-        return;
+    // 문자열 집합 S에 문자열 입력
+    for (int i = 0; i < N; i++) {
+        string str;
+        cin >> str;
+        S.insert(str);
     }
 
-    for (int i = 0; i < 4; i++)
-    {
-        if (op[i] > 0)
-        {
-            op[i]--;
-            if (i == 0)
-                dfs(idx + 1, val + A[idx], A, op, N);
-            else if (i == 1)
-                dfs(idx + 1, val - A[idx], A, op, N);
-            else if (i == 2)
-                dfs(idx + 1, val * A[idx], A, op, N);
-            else
-                dfs(idx + 1, val / A[idx], A, op, N);
-            op[i]++;
-        }
+    // 문자열 집합 S에 문자열이 있는지 확인
+    int cnt = 0;
+    for (int i = 0; i < M; i++) {
+        string str;
+        cin >> str;
+        if (S.find(str) != S.end()) cnt++;  // 문자열이 있으면 cnt 증가
     }
+
+    cout << cnt << endl;
 }
+
