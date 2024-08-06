5ea5974e82aac3aec6c6f728b85af8a95f6a7f60
isaac <gladisaac5441@gmail.com>
2024-08-06 10:24:37 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,27 @@
-// boj 14425 문자열 집합
-#pragma GCC optimize("O3")
-
-#include <bits/stdc++.h>
-
-#define endl '\n'
-#define ll long long
+#include <iostream>
 using namespace std;
 
-// 함수 선언
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    string s;
+    cin >> s;
 
-// 전역 변수
+    int a = 0, ans = s.size();
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    // count 'a'
+    for(auto c : s)
+        if(c=='a') a++;
 
-    // 입력
-    int N, M;
-    cin >> N >> M;
-    set<string> S;  // 문자열 집합
-
-    // 문자열 집합 S에 문자열 입력
-    for (int i = 0; i < N; i++) {
-        string str;
-        cin >> str;
-        S.insert(str);
+    // sliding window, cnt: count 'a', temp: count 'b'
+    for(int i=0; i<s.size(); i++){
+        int cnt = a, temp = 0;
+        for(int j=i; j<i+s.size(); j++){
+            if(cnt==0) break;   //
+            if(s[j%s.size()]=='b') temp++, cnt--;
+            else cnt--;
+        }
+        ans = min(ans,temp);
     }
-
-    // 문자열 집합 S에 문자열이 있는지 확인
-    int cnt = 0;
-    for (int i = 0; i < M; i++) {
-        string str;
-        cin >> str;
-        if (S.find(str) != S.end()) cnt++;  // 문자열이 있으면 cnt 증가
-    }
-
-    cout << cnt << endl;
+    cout << ans << "\n";
+    return 0;
 }
-
