dc12622b26796d2c22f982a02055b25b088e3530
isaac <gladisaac5441@gmail.com>
2024-08-10 08:57:40 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,67 +1,18 @@
-#include <bits/stdc++.h>
-
-#define endl '\n'
-#define ll long long
+#include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0);
+    cout.tie(0);
 
-    int T;
-    cin >> T;
+    string A, B, C;
 
-    for (int i = 0; i < T; i++) {
-        string s;
-        cin >> s;
+    cin >> A >> B >> C;
 
-        int left = 0, right = s.size() - 1;
-        int ans = 0;  // 초기값 0: 회문
-
-        bool isoneCheck = false;
-
-        while (left < right) {
-            if (s[left] == s[right]) {
-                left++;
-                right--;
-            } else {
-                // 유사 회문 체크
-                if (!isoneCheck) {
-                    if (s[left + 1] == s[right] && left + 1 <= right) {
-                        int temp_left = left + 1, temp_right = right;
-                        while (temp_left < temp_right && s[temp_left] == s[temp_right]) {
-                            temp_left++;
-                            temp_right--;
-                        }
-                        if (temp_left >= temp_right) {
-                            ans = 1;
-                            break;
-                        }
-                    }
-
-                    if (s[left] == s[right - 1] && left <= right - 1) {
-                        int temp_left = left, temp_right = right - 1;
-                        while (temp_left < temp_right && s[temp_left] == s[temp_right]) {
-                            temp_left++;
-                            temp_right--;
-                        }
-                        if (temp_left >= temp_right) {
-                            ans = 1;
-                            break;
-                        }
-                    }
-
-                    ans = 2;
-                    break;
-                } else {
-                    ans = 2;
-                    break;
-                }
-            }
-        }
-
-        cout << ans << endl;
-    }
+    cout << stoi(A) + stoi(B) - stoi(C) << '\n';
+    cout << stoi(A + B) - stoi(C) << '\n';
 
     return 0;
 }
