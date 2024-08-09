b807534e1251f7bda57401eba874d2da599f81e9
isaac <gladisaac5441@gmail.com>
2024-08-09 11:55:00 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -8,24 +8,58 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    string s;
-    cin >> s;
+    int T;
+    cin >> T;
 
-    string bomb;
-    cin >> bomb;
+    for (int i = 0; i < T; i++) {
+        string s;
+        cin >> s;
 
-    string ans;
+        int left = 0, right = s.size() - 1;
+        int ans = 0;  // 초기값 0: 회문
 
-    for (char c : s) {
-        ans.push_back(c);
-        if (ans.size() >= bomb.size() && ans.substr(ans.size() - bomb.size(), bomb.size()) == bomb) {
-            ans.erase(ans.size() - bomb.size(), bomb.size());   //
+        bool isoneCheck = false;
+
+        while (left < right) {
+            if (s[left] == s[right]) {
+                left++;
+                right--;
+            } else {
+                // 유사 회문 체크
+                if (!isoneCheck) {
+                    if (s[left + 1] == s[right] && left + 1 <= right) {
+                        int temp_left = left + 1, temp_right = right;
+                        while (temp_left < temp_right && s[temp_left] == s[temp_right]) {
+                            temp_left++;
+                            temp_right--;
+                        }
+                        if (temp_left >= temp_right) {
+                            ans = 1;
+                            break;
+                        }
+                    }
+
+                    if (s[left] == s[right - 1] && left <= right - 1) {
+                        int temp_left = left, temp_right = right - 1;
+                        while (temp_left < temp_right && s[temp_left] == s[temp_right]) {
+                            temp_left++;
+                            temp_right--;
+                        }
+                        if (temp_left >= temp_right) {
+                            ans = 1;
+                            break;
+                        }
+                    }
+
+                    ans = 2;
+                    break;
+                } else {
+                    ans = 2;
+                    break;
+                }
+            }
         }
-    }
 
-    if (ans.empty()) {
-        cout << "FRULA" << endl;
-    } else {
         cout << ans << endl;
     }
 
