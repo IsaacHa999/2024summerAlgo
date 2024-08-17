ec0e5ae28cfae74cfbcaa996e39402788d24439d
isaac <gladisaac5441@gmail.com>
2024-08-17 20:41:43 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,67 +1,47 @@
+// boj 1316 그룹 단어 체커
+#pragma GCC optimize("O3")
+
 #include <bits/stdc++.h>
 
 #define endl '\n'
 #define ll long long
 using namespace std;
 
-int main() {
+// 함수 선언
+bool isGroupWord(string word);
+
+// 전역 변수
+
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int T;
-    cin >> T;
+    // 변수 선언, 초기화
+    int N;
+    cin >> N;
+    vector<string> words(N);
+    int count = 0;
 
-    for (int i = 0; i < T; i++) {
-        string s;
-        cin >> s;
+    for (int i = 0 ;i < N; i++)
+        cin >> words[i];
 
-        int left = 0, right = s.size() - 1;
-        int ans = 0;  // 초기값 0: 회문
-
-        bool isoneCheck = false;
-
-        while (left < right) {
-            if (s[left] == s[right]) {
-                left++;
-                right--;
-            } else {
-                // 유사 회문 체크
-                if (!isoneCheck) {
-                    if (s[left + 1] == s[right] && left + 1 <= right) {
-                        int temp_left = left + 1, temp_right = right;
-                        while (temp_left < temp_right && s[temp_left] == s[temp_right]) {
-                            temp_left++;
-                            temp_right--;
-                        }
-                        if (temp_left >= temp_right) {
-                            ans = 1;
-                            break;
-                        }
-                    }
-
-                    if (s[left] == s[right - 1] && left <= right - 1) {
-                        int temp_left = left, temp_right = right - 1;
-                        while (temp_left < temp_right && s[temp_left] == s[temp_right]) {
-                            temp_left++;
-                            temp_right--;
-                        }
-                        if (temp_left >= temp_right) {
-                            ans = 1;
-                            break;
-                        }
-                    }
-
-                    ans = 2;
-                    break;
-                } else {
-                    ans = 2;
-                    break;
-                }
-            }
-        }
-
-        cout << ans << endl;
+    for (int i = 0; i < N; i++) {
+        if(isGroupWord(words[i]))
+            count++;
     }
 
-    return 0;
+    // 출력
+    cout << count << endl;
+}
+
+// 함수 정의
+bool isGroupWord(string word) {
+    vector<bool> check(26, false);
+    for (int i = 0; i < word.size(); i++) {
+        if (check[word[i] - 'a']) return false;
+        check[word[i] - 'a'] = true;
+        while (i + 1 < word.size() && word[i] == word[i + 1]) i++;
+    }
+    return true;
 }
