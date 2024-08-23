6bfb506e4c123411765f4178a5561990b357342a
isaac <gladisaac5441@gmail.com>
2024-08-23 11:20:49 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// boj 11651 좌표 정렬하기 2
+// boj 28702 FizzBuzz
 #pragma GCC optimize("O3")
 
 #include <bits/stdc++.h>
@@ -17,20 +17,58 @@ int main()
     cin.tie(nullptr);
 
     // 변수 선언, 초기화
-    int N;
-    cin >> N;
-    vector<pair<int, int>> v(N);
-    for (int i = 0; i < N; i++) {
-        cin >> v[i].second >> v[i].first;   // y, x
+    vector<pair<string,bool>> v(3); // 숫자면 true, 문자면 false
+    for (int i = 0; i < 3; i++) {
+        cin >> v[i].first;
+        v[i].second = false;
     }
-
     // 로직
-    sort(v.begin(), v.end());
-
-    // 출력
-    for (int i = 0; i < N; i++) {
-        cout << v[i].second << ' ' << v[i].first << endl;
+    // v 중에 숫자가 있는지 확인
+    for (int i = 0; i < 3; i++) {
+        if (v[i].first == "Fizz" || v[i].first == "Buzz" || v[i].first == "FizzBuzz") {
+            v[i].second = false;
+        }
+        else {
+            v[i].second = true;
+        }
     }
+
+    // 숫자를 찾고, 인덱스와, 값 구한다
+    int num = 0, index = 0;
+    for (int i = 0; i < 3; i++) {
+        if(v[i].second == true) {
+            num = stoi(v[i].first);
+            index = i;
+            break;
+        }
+    }
+    // 다음 출력 값 구하기
+    int number = 0;         // 다음 숫자
+    if (index == 0)
+        number = num + 3;
+    else if (index == 1)
+        number = num + 2;
+    else if (index == 2)
+        number = num + 1;
+
+    // // 디버깅
+    // cout << "index : " << index << endl;
+    // cout << "number : " << number << endl;
+
+    // number FizzBuzz 판별
+    if (number % 3 == 0 && number % 5 == 0) {
+        cout << "FizzBuzz" << endl;
+    }
+    else if (number % 3 == 0) {
+        cout << "Fizz" << endl;
+    }
+    else if (number % 5 == 0) {
+        cout << "Buzz" << endl;
+    }
+    else {
+        cout << number << endl;
+    }
+
 }
 
 // 함수 정의
