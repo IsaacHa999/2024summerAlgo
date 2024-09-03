2d644560818ee9808659e13d9f446603a2940bba
modneyTree <102788008+modneyTree@users.noreply.github.com>
2024-09-03 09:16:09 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-// boj
 #pragma GCC optimize("O3")
 
 #include <bits/stdc++.h>
@@ -7,21 +6,41 @@
 #define ll long long
 using namespace std;
 
-// 함수 선언
-
-// 전역 변수
-
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    // 변수 선언, 초기화
-    long long a, b, c;
-    cin >> a >> b >> c;
+    // 입력받은 식
+    string s;
+    cin >> s;
 
-    // 로직
-    cout << a + b + c ;
+    // '-'로 식을 나눈다
+    stringstream ss(s);
+    string token;
+    vector<string> terms;
+
+    while (getline(ss, token, '-')) {
+        terms.push_back(token);
+    }
+
+    // 첫 번째 그룹은 더해준다
+    int sum = 0;
+    stringstream first(terms[0]);
+    while (getline(first, token, '+')) {
+        sum += stoi(token);
+    }
+
+    // 나머지 그룹들은 빼준다
+    for (int i = 1; i < terms.size(); ++i) {
+        stringstream ss(terms[i]);
+        while (getline(ss, token, '+')) {
+            sum -= stoi(token);
+        }
+    }
+
+    // 결과 출력
+    cout << sum << endl;
+
+    return 0;
 }
-
-// 함수 정의
