42776b02c0dd7514888a3869919beb9b9c24c8f5
modneyTree <102788008+modneyTree@users.noreply.github.com>
2024-09-05 09:13:53 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+// boj 1764 듣보잡
+/*
+ * 자료구조: set, map, unordered_map, unordered_set
+ * 1. set: 중복을 허용하지 않는 자료구조
+ * 2. map: key-value 쌍으로 이루어진 자료구조
+ * 3. unordered_map: map과 같은 기능을 하지만, 해시함수를 사용하여 더 빠르게 동작
+ * 4. unordered_set: set과 같은 기능을 하지만, 해시함수를 사용하여 더 빠르게 동작
+ *
+ * 또는 vector를 이용하여 풀 수 있음 (이 문제는 vector를 이용하여 풀이), binary_search를 이용하여 풀이
+ */
 #pragma GCC optimize("O3")
 
 #include <bits/stdc++.h>
@@ -6,41 +16,42 @@
 #define ll long long
 using namespace std;
 
+// 함수 선언
+
+// 전역 변수
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    // 입력받은 식
-    string s;
-    cin >> s;
+    // 변수 선언, 초기화
+    int N, M;
+    cin >> N >> M;
+    vector<string> v1(N), v2(M);
+    int cnt = 0;
+    vector<string> ans;
+    for (int i = 0; i < N; i++)
+        cin >> v1[i];
 
-    // '-'로 식을 나눈다
-    stringstream ss(s);
-    string token;
-    vector<string> terms;
+    // 로직
+    sort(v1.begin(), v1.end());
 
-    while (getline(ss, token, '-')) {
-        terms.push_back(token);
-    }
-
-    // 첫 번째 그룹은 더해준다
-    int sum = 0;
-    stringstream first(terms[0]);
-    while (getline(first, token, '+')) {
-        sum += stoi(token);
-    }
-
-    // 나머지 그룹들은 빼준다
-    for (int i = 1; i < terms.size(); ++i) {
-        stringstream ss(terms[i]);
-        while (getline(ss, token, '+')) {
-            sum -= stoi(token);
+    for (int i = 0; i < M; i++) {
+        cin >> v2[i];
+        if (binary_search(v1.begin(), v1.end(), v2[i])) {
+            cnt++;
+            ans.push_back(v2[i]);
         }
     }
 
-    // 결과 출력
-    cout << sum << endl;
+    // 출력
+    cout << cnt << endl;
+    sort(ans.begin(), ans.end());
+    for (auto& i : ans)
+        cout << i << endl;
 
-    return 0;
+
 }
+
+// 함수 정의
