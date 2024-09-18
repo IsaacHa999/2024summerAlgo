2cb107680e7f613f7346dfaecf7b8da1dc99c480
modneyTree <102788008+modneyTree@users.noreply.github.com>
2024-09-18 18:13:28 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
-// boj 10807 개수 세기
-// 구현
+// boj 2667 단지번호붙이기
 #pragma GCC optimize("O3")
 
 #include <bits/stdc++.h>
@@ -10,6 +9,7 @@ using namespace std;
 
 // 함수 선언
 
+
 // 전역 변수
 
 int main()
@@ -18,20 +18,55 @@ int main()
     cin.tie(nullptr);
 
     // 변수 선언, 초기화
-    int n, v, cnt = 0;
+    int n;
     cin >> n;
-    vector<int> arr(n);
-
+    vector<string> map(n);
     for (int i = 0; i < n; i++)
-        cin >> arr[i];
+        cin >> map[i];
 
-    // 입력 및 처리
-    cin >> v;
+    int cnt = 0;    // 단지 수
+    vector<int> ans;    // 단지별 집 수
 
-    cnt = count(arr.begin(), arr.end(), v);
-
-    cout << cnt << endl;
     // 로직
+    vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    queue<pair<int, int>> q;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+        {
+            if (map[i][j] == '1')
+            {
+                cnt++;
+                q.push({i, j});
+                map[i][j] = '0';
+                int house = 1;  // 집 수
+                while (!q.empty())
+                {
+                    auto [x, y] = q.front();
+                    q.pop();
+                    for (auto [dx, dy] : dir)
+                    {
+                        int nx = x + dx;
+                        int ny = y + dy;
+                        if (nx < 0 || nx >= n || ny < 0 || ny >= n)
+                            continue;
+                        if (map[nx][ny] == '1')
+                        {
+                            house++;
+                            q.push({nx, ny});
+                            map[nx][ny] = '0';
+                        }
+                    }
+                }
+                ans.push_back(house);
+            }
+        }
+
+    // 출력
+    cout << cnt << endl;
+    sort(ans.begin(), ans.end());
+    for (int i = 0; i < ans.size(); i++)
+        cout << ans[i] << endl;
+
 }
 
 // 함수 정의
