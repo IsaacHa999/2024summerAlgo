2a614f250ef254dd31f90c6bbbd1cf920870428c
modneyTree <102788008+modneyTree@users.noreply.github.com>
2024-09-24 08:57:58 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// boj 10828 스택
+// boj 2743 단어 길이 재기
 #pragma GCC optimize("O3")
 
 #include <bits/stdc++.h>
@@ -17,38 +17,12 @@ int main()
     cin.tie(nullptr);
 
     // 변수 선언, 초기화
-    int n;
-    cin >> n;
-    stack<int> s;
+    string s;
+    cin >> s;
+
+    cout << s.size() << endl;
 
     // 로직
-    for (int i = 0; i < n; i++) {
-        string cmd;
-        cin >> cmd;
-
-        if (cmd == "push") {
-            int num;
-            cin >> num;
-            s.push(num);
-        } else if (cmd == "pop") {
-            if (s.empty()) {
-                cout << -1 << endl;
-            } else {
-                cout << s.top() << endl;
-                s.pop();
-            }
-        } else if (cmd == "size") {
-            cout << s.size() << endl;
-        } else if (cmd == "empty") {
-            cout << s.empty() << endl;
-        } else if (cmd == "top") {
-            if (s.empty()) {
-                cout << -1 << endl;
-            } else {
-                cout << s.top() << endl;
-            }
-        }
-    }
 }
 
 // 함수 정의
