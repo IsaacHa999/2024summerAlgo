85a0b490d53c62969665eb8e46bcbd69af2e1f26
modneyTree <102788008+modneyTree@users.noreply.github.com>
2024-10-04 11:46:30 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,43 +1,59 @@
-// boj 11053 가장 긴 증가하는 부분 수열
-#pragma GCC optimize("O3")
+// boj 9466 텀 프로젝트
+// dfs로 사이클 찾기
 
-#include <bits/stdc++.h>
-
-#define endl '\n'
-#define ll long long
+#include <iostream>
+#include <queue>
+#include <cstring>
+#define MAX 100001
 using namespace std;
 
-// 함수 선언
+int t, n;
+int graph[MAX];
+bool visited[MAX];
+bool done[MAX];
+int cnt;
+void hasCycle( int node) {
 
-// 전역 변수
+    visited[node] = true;
+    int next = graph[node];
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
 
-    // 변수 선언, 초기화
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    vector<int> dp(n, 1);   // dp[i] = i번째 수를 마지막으로 하는 가장 긴 증가하는 부분 수열의 길이
-    for (int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    // 입력 및 처리
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            if (arr[i] > arr[j])
-                dp[i] = max(dp[i], dp[j] + 1);
-        }
+    if (!visited[next]) {
+        hasCycle( next);
     }
-
-    // 출력
-    cout << *max_element(dp.begin(), dp.end()) << endl;
-
-    // 로직
+    else if (!done[next]) {//방문은 했지만 아직 사이클이 아니라면 next까지 포함해서 사이클 완성
+        //자기 자신을 포함한 팀의 멤버를 카운트
+        for (int i = next; i != node; i = graph[i]) {
+            cnt++;
+        }
+        cnt++;
+    }
+    done[node] = true;
 }
 
-// 함수 정의
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        for (int i = 1; i <= n; i++) {
+            cin >> graph[i];
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                hasCycle(i);
+            }
+        }
+        cout << n-cnt << '\n';
+        cnt = 0;
+        memset(visited, false, sizeof(visited));
+        memset(done, false, sizeof(done));
+    }
+
+
+    return 0;
+}
