5cc0084545e3d233c6f7bb36f844d1eed2abbe94
modneyTree <102788008+modneyTree@users.noreply.github.com>
2024-12-13 11:42:11 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	main.cpp

--- main.cpp
+++ main.cpp
@@ -1,69 +1,41 @@
+// boj
 #pragma GCC optimize("O3")
 
 #include <bits/stdc++.h>
 
 #define endl '\n'
+#define ll long long
 using namespace std;
 
-// Direction arrays for moving up, down, left, right
-int dx[4] = {1, 0, -1, 0};
-int dy[4] = {0, 1, 0, -1};
+// 함수 선언
+
+// 전역 변수
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    // Variable declaration and initialization
-    int n, m;
-    cin >> n >> m;
+    // 변수 선언, 초기화
+    int x,y,w,h;
+    cin >> x >> y >> w >> h;
+    int min = 10001;
 
-    vector<vector<char>> campus(n, vector<char>(m));
-    pair<int, int> hero; // Position of 'I'
+    // logic
+    // 0    x    w
+    // 0    y   h
+    if (x < min)
+        min = x;
+    if (w-x < min)
+        min = w - x;
+    if (y < min)
+        min = h;
+    if (h - y < min)
+        min = h - y;
 
-    // Input campus data
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> campus[i][j];
+    cout << min << endl;
 
-            if (campus[i][j] == 'I') { // Find hero's position
-                hero = {i, j};
-            }
-        }
-    }
-
-    int countPerson = 0;
-
-    // BFS
-    queue<pair<int, int>> q;
-    vector<vector<int>> visited(n, vector<int>(m, 0)); // Initialize visited array
-    q.push(hero);
-    visited[hero.first][hero.second] = 1;
-
-    while (!q.empty()) { // Use !q.empty() for BFS
-        pair<int, int> front = q.front();
-        q.pop();
-
-        // Check if current position contains a person 'P'
-        if (campus[front.first][front.second] == 'P')
-            countPerson++;
-
-        // Explore 4 directions
-        for (int i = 0; i < 4; i++) {
-            int x = front.first + dx[i];
-            int y = front.second + dy[i];
-
-            // Check bounds and if not visited and not a wall 'X'
-            if (x >= 0 && x < n && y >= 0 && y < m && !visited[x][y] && campus[x][y] != 'X') {
-                q.push({x, y});
-                visited[x][y] = 1;
-            }
-        }
-    }
-
-    // Output the result
-    if (countPerson > 0)
-        cout << countPerson << endl;
-    else
-        cout << "TT" << endl; // Output "TT" if no person 'P' is found
+    // 로직
 }
+
+// 함수 정의
